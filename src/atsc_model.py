c1d4bf02d606dd1aa43cd6c7374c9c127b959a9a
Minh-Hau Tran <tranminhhau9a2thcsttlp@gmail.com>
2026-04-06 12:31:03 +0700

    Update atsc_model.py

--- src/atsc_model.py
+++ src/atsc_model.py
@@ -1,7 +1,7 @@
-from transformers import BertForTokenClassification
+from transformers import BertForSequenceClassification
 
-def build_ate_model(num_labels=3):
-    return BertForTokenClassification.from_pretrained(
+def build_atsc_model(num_labels=3):
+    return BertForSequenceClassification.from_pretrained(
         "bert-base-uncased",
         num_labels=num_labels
     )
